b46ba7a4177d2607eefce204dc61d406eeb45318
kd101-44 <kalvikdas@gmail.com>
2026-02-13 12:40:57 -0600

    Testing Code on lab1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -8,7 +8,7 @@ with your name. Then, save the code, add it to the staging area, and commit it t
 def main():
     print("Hello World!")
 
-    name = "Kalvik Das" # TODO: Insert your name between the double quotes
+    name = "Kalvik Dasd" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
     print ("Hey y'all, I'm Kalvik! I'm a Junior from Dallas, Texas, and I'm interested in mechanical/aerospace engineering. I was part of the JHU EEI summer program last year, and I currently participate in FTC (FIRST Tech Challenge). I'm a founder of my school's engineering club, where we work on STEM projects (like building pasta trusses) and tutor middle schoolers on STEM topics. I enjoy hearing about new cultures, and I love learning math, physics, and chemistry! In my spare time, I simrace, catch up on Formula 1, or binge-watch my favorite series, Dark!")
