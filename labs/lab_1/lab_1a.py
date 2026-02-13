de2f56bdbcfef2f5c09b0e3f0b6cb77c309a8f13
kd101-44 <kalvikdas@gmail.com>
2026-02-13 12:29:39 -0600

    Added intro to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Kalvik Das" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print ("Hey y'all, I'm Kalvik! I'm a Junior from Dallas, Texas, and I'm interested in mechanical/aerospace engineering. I was part of the JHU EEI summer program last year, and I currently participate in FTC (FIRST Tech Challenge). I'm a founder of my school's engineering club, where we work on STEM projects (like building pasta trusses) and tutor middle schoolers on STEM topics. I enjoy hearing about new cultures, and I love learning math, physics, and chemistry! In my spare time, I simrace, catch up on Formula 1, or binge-watch my favorite series, Dark!")
 if __name__ == "__main__":
     main()
