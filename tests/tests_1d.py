bd1f04c5ac445645bc14dd32a20dd60bf1a20412
kd101-44 <kalvikdas@gmail.com>
2026-03-17 22:44:12 -0500

    Updated tests_ed.py to prevent runtime error.

--- tests/tests_1d.py
+++ tests/tests_1d.py
@@ -1,17 +1,23 @@
 import pytest
 from labs.lab_1.lab_1d import two_sum
 
+def check_two_sum_result(nums, target, result):
+    assert len(result) == 2
+    i, j = result
+    assert i != j
+    assert nums[i] + nums[j] == target
+
 def test_two_sum_basic():
-    assert two_sum([2, 7, 11, 15], 9) == [0, 1]
+    check_two_sum_result([2, 7, 11, 15], 9, two_sum([2, 7, 11, 15], 9))
 
 def test_two_sum_different_order():
-    assert two_sum([3, 2, 4], 6) == [1, 2]
+    check_two_sum_result([3, 2, 4], 6, two_sum([3, 2, 4], 6))
 
 def test_two_sum_negative_numbers():
-    assert two_sum([-1, -2, -3, -4, -5], -8) == [2, 4]
+    check_two_sum_result([-1, -2, -3, -4, -5], -8, two_sum([-1, -2, -3, -4, -5], -8))
 
 def test_two_sum_large_numbers():
-    assert two_sum([1000000, 500000, 1500000], 2000000) == [0, 2]
+    check_two_sum_result([1000000, 500000, 1500000], 2000000, two_sum([1000000, 500000, 1500000], 2000000))
 
 def test_two_sum_adjacent():
-    assert two_sum([1, 3, 2, 4], 5) == [1, 3]
+    check_two_sum_result([1, 3, 2, 4], 5, two_sum([1, 3, 2, 4], 5))
